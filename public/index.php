3192ca33608fc25e3c62eddcf96e8b04935cd1b6
Philippe Gerhardt <philippe@sutoiku.com>
2025-05-16 12:39:19 +0200

    Update to handle loc and action

--- public/index.php
+++ public/index.php
@@ -2,25 +2,39 @@
 
 include '../bootstrap.php';
 
+function my_extract(array $data) {
+  foreach ($data as $key => $value) {
+      $GLOBALS[$key] = $value;
+  }
+}
+
 $loc = filter_input(INPUT_GET, "loc");
-$action = filter_input(INPUT_POST, "action");
-var_dump($loc, $action);
+$action = filter_input(INPUT_GET, "action");
+$id = filter_input(INPUT_GET, "id");
 
 $controller = null;
 
 switch ($loc) {
     case "tag":
-        include(BASE_PATH."src/controllers/TagController.php");
-        $controller = new TagController($actoin);
-        // $controller->index();
-        // $controller->show();
+        include(__DIR__ . "/../app/controllers/TagController.php");
+        $controller = new TagController($action);
 
         break;
     case "note":
-        include("app/controllers/NoteController.php");
-        $controller = new NoteControleur($action);
+        include(__DIR__ . "/../app/controllers/NoteController.php");
+        $controller = new NoteController($action);
+        break;
+
+    default:
+        include(__DIR__ . "/../app/controllers/NoteController.php");
+        $controller = new NoteController("index");
         break;
 }
 
-$controller->run();
+$data = $controller->run($id);
+extract($data);
 
+// Include layout
+include "template/header.php";
+include $controller->getView();
+include "template/footer.php";
